bd17a100e15c3ee432f4b127b220eed9d22ad0ee
Globski <gloria.ogunsemore@gmail.com>
2023-11-30 07:04:32 -0800

    Add Task 8: Print string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    for char in str:
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - 32)
+        else:
+            uppercase_char = char
+        print("{}".format(uppercase_char), end="")
+    print("")
