45774f07a5b10ef509bca70215c02b22fccfcdfd
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-30 04:08:24 -0800

    Fix sign adjustment in last_digit for negative numbers.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,9 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 msg = "Last digit of {} is {} and is".format(number, last_digit)
 
+if number < 0:
+    last_digit = -last_digit
+
 if last_digit > 5:
     print("{} greater than 5".format(msg))
 elif last_digit == 0:
