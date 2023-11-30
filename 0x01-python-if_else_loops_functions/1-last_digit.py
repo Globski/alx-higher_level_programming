62224bfc8d3cdb8ca8f95053437a0f5fa0a820aa
Globski <gloria.ogunsemore@gmail.com>
2023-11-30 03:34:05 -0800

    This commit implements the solution for Task 1. The `1-last_digit.py` script generates a random signed number and prints the last digit, along with additional information based on specific conditions.
    - Used the modulus operator to find the last digit of the generated number.
    - Constructed a message indicating the last digit and its relationship to 5 and 0.
    - Implemented if, elif, and else statements for different scenarios.
    
    Tested the script multiple times to ensure accurate and expected output for various random numbers.
    
    - Followed the Pycodestyle style guide to maintain code consistency.
    - No changes were made to the original code structure provided.

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+last_digit = abs(number) % 10
+message = "Last digit of {} is {} and is".format(number, last_digit)
+
+if last_digit > 5:
+    print("{} greater than 5".format(message))
+elif last_digit == 0:
+    print("{} 0".format(message))
+else:
+    print("{} less than 6 and not 0".format(message))
