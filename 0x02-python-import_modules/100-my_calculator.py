64aebe5742731c0962ca13a6a650acaddda10092
Globski <gloria.ogunsemore@gmail.com>
2023-11-30 23:51:43 -0800

    Add solution for Task 6: Build my own calculator!

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+        from calculator_1 import add, sub, mul, div
+
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
