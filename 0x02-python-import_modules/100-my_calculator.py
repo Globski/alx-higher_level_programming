41946362e8704462a717536b4ad6ada6d7140426
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-30 23:54:08 -0800

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
         from calculator_1 import add, sub, mul, div
-            from sys import argv
+        from sys import argv
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
