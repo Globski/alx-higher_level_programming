fd884217caf27b66cfe6048377e93691f9d08b27
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-30 23:07:36 -0800

    Fix line length and improve readability in 2-args.py.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,12 @@ if __name__ == "__main__":
     from sys import argv
 
     count = len(argv) - 1
-    print("{} {}{}".format(count, "argument" if count == 1 else "arguments", ":" if count > 0 else "."))
+    plural = "s" if count != 1 else ""
+    punctuation = ":" if count > 0 else "."
+
+    print("{} {}{}".format(count, "argument" + plural, punctuation))
 
     for i in range(1, count + 1):
         print("{}: {}".format(i, argv[i]))
+
+
