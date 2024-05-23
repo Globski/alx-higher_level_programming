292a815041fb337deb6b1a91c7415a80441e8935
gboski <gloria.ogunsemore@gmail.com>
2024-05-23 14:12:16 -0700

    Add SQL script to create table second_table and insert multiple rows with attributes id, name and score.

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Creates a table second_table and inserts multiple rows with attributes id, name and score.
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+
