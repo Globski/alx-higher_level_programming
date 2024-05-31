5397d2849a37c3c04dc840eb3a90e00c25bfd5dd
Gloriana <141042662+Globski@users.noreply.github.com>
2024-05-31 15:15:38 -0700

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,14 @@
 -- Convert hbtn_0c_0 database, first_table table, and name field to UTF8
 
 -- Convert database to UTF8
-USE hbtn_0c_0
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- Convert table to UTF8
+USE hbtn_0c_0;
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Convert field to UTF8
-ALTER TABLE first_table CHANGE name name VARCHAR(256)
+ALTER TABLE first_table CHANGE name VARCHAR(256)
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
