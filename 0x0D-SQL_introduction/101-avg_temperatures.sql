59f77525a28c3acd8e1a7d8ec012232c2ab7cd76
Gloriana <141042662+Globski@users.noreply.github.com>
2024-05-31 15:32:42 -0700

    Update 101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,8 +1,6 @@
--- Displays the 3 cities with the highest average temperatures between July and August. 
+-- Display the average temperature (Fahrenheit) by city
 -- ordered by temperature (descending)
-SELECT city, AVG(temperature) AS avg_temp
-FROM temperatures
-WHERE MONTH(date) IN (7, 8)
-GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
