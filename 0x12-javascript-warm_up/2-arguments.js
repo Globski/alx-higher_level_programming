d79ecf2fbb6e49db30a4cfccfc6cd1b7527c7b0e
Gloriana <141042662+Globski@users.noreply.github.com>
2024-06-19 19:10:55 -0700

    Fix semistandard issues in 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.length - 2;
 if (args === 0) {
-	console.log("No argument");
+  console.log('No argument');
 } else if (args === 1) {
-	console.log("Argument found");
+  console.log('Argument found');
 } else {
-	console.log("Arguments found");
+  console.log('Arguments found');
 }
