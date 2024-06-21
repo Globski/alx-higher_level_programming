38173e6f112f4ef91c42d09a58ce45dd30bbdfb9
gboski <gloria.ogunsemore@gmail.com>
2024-06-20 19:38:39 -0700

    Create a new dictionary with keys sorted by occurrence

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const [key, value] of Object.entries(dict)) {
+  if (!newDict[value]) {
+    newDict[value] = [];
+  }
+  newDict[value].push(key);
+}
+console.log(newDict);
