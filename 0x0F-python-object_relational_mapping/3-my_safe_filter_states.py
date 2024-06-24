827f1ac8a7d59f2d6ac6c3041db2f6b1a707b94b
gboski <gloria.ogunsemore@gmail.com>
2024-06-23 17:40:04 -0700

    Add  to safely list states matching user input, preventing SQL injections.

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa 
+where name matches the argument.
+Safe from SQL injections.
+
+This script takes four arguments:
+Args:
+        username (str): The MySQL username.
+        password (str): The MySQL password.
+        db_name (str): The name of the database.
+        state_name (str): The name of the state to search for.
+
+Usage:
+    ./3-my_safe_filter_states.py hbtn_0e_0_usa 'Arizona'
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    cur.execute(query, (sys.argv[4],))
+
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+
+    cur.close()
+    db.close()
