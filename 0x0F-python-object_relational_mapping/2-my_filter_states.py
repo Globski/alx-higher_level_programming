6b79fe241d967b166ea09b998c482fa1056d90bf
Gloriana <141042662+Globski@users.noreply.github.com>
2024-06-23 18:08:31 -0700

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' "
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}'"
     "ORDER BY id ASC".format(
         sys.argv[4]
     )
