af8bee98e2f0d7a3e7f97735e2a94b2c8a901f6e
Gloriana <141042662+Globski@users.noreply.github.com>
2024-06-23 17:48:18 -0700

    Removed trailing whitespace from 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table of hbtn_0e_0_usa 
+Displays all values in the states table of hbtn_0e_0_usa
 where name matches the argument.
 Safe from SQL injections.
 
