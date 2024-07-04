5b839220c68970e935253d04170efe4b9c4c049c
gboski <gloria.ogunsemore@gmail.com>
2024-07-04 16:56:49 -0700

    Trigger server response with custom PUT request

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Sends a request to 0.0.0.0:5000/catch_me that causes the server to respond with "You got me!" in the body of the response.
+curl -sLX PUT "$1/catch_me" -d "user_id=98" -H "Origin: HolbertonSchool"
+
