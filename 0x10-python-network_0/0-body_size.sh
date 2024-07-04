37a28feb26e6be870563aee3976fda27517db446
Gloriana <141042662+Globski@users.noreply.github.com>
2024-07-04 16:04:27 -0700

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 
 # Take a URL as an argument
 # Send a request to the URL, and display the size of the body of the response.
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+curl -s "$1" | wc -c
