d90cc02348cb03e73d96bad153232ab2be0be5c9
gboski <gloria.ogunsemore@gmail.com>
2024-07-04 17:22:10 -0700

    Implement find_peak function to identify peak element in unsorted list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
+
+def find_peak(list_of_integers):
+    """
+    Finds a peak element in the list of unsorted integers.
+    """
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
