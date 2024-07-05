458921acdd43db328621d33f7753b71735faffd5
Gloriana <141042662+Globski@users.noreply.github.com>
2024-07-04 17:01:32 -0700

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a request to 0.0.0.0:5000/catch_me that causes the server to respond with "You got me!" in the body of the response.
-curl -sLX PUT "$1/catch_me" -d "user_id=98" -H "Origin: HolbertonSchool"
-
+curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" "$1/catch_me"
