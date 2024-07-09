96aa0f2efb628e14cc070173aefaec1d9271a2f1
globski <gloriaogunsemore@gmail.com>
2024-07-09 14:40:31 -0700

    Send a POST request to a given URL with an email as a parameter

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the
+response (decoded in utf-8).
+Usage: ./2-post_email.py <URL> <email>
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    request = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(request) as response:
+        body = response.read().decode('utf-8')
+
+        print(body)
