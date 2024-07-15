adde549682b9855c83fa873994a4662821bf0565
gboski <gloria.ogunsemore@gmail.com>
2024-07-15 10:39:02 -0700

    Create database hbtn_0d_2 and user user_0d_2 with SELECT privilege

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,10 @@
+-- Create user user_0d_1 and grant all privileges
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
+
