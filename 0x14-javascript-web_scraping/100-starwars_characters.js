21dc84c0eb822e7e1dbee2e7168ae0dfb216e5a2
globski <gloriaogunsemore@gmail.com>
2024-07-30 08:57:08 -0700

    Print all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    data.characters.forEach(character => {
+      console.log(character);
+    });
+  }
+});
