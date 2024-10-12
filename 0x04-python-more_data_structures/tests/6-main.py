dbfc1898f03d46d024b123835c7b8917d3e4a14f
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-12 15:00:50 -0700

    Add test file 6-main.py for print_sorted_dictionary function.

--- 0x04-python-more_data_structures/tests/6-main.py
+++ 0x04-python-more_data_structures/tests/6-main.py
@@ -3,4 +3,3 @@ print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_d
 
 a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
 print_sorted_dictionary(a_dictionary)
-
