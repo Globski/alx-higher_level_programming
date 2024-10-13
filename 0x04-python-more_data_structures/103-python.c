053aad8fe14d48d304c2470ff69be8fa24b5ee3c
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-12 17:52:11 -0700

    Update 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -9,7 +9,7 @@ void print_python_bytes(PyObject *p);
  * @p: A PyObject list object.
  */
 void print_python_list(PyObject *p) {
-    Py_ssize_t size, alloc, i;
+    Py_ssize_t size, i;
     PyObject *item;
 
     if (!PyList_Check(p)) {
@@ -18,19 +18,15 @@ void print_python_list(PyObject *p) {
     }
 
     size = PyList_Size(p);
-    alloc = ((PyListObject *)p)->allocated;
-
     printf("[*] Python list info\n");
     printf("[*] Size of the Python List = %zd\n", size);
-    printf("[*] Allocated = %zd\n", alloc);
+    printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
 
     for (i = 0; i < size; i++) {
-        item = PyList_GetItem(p, i);  // Safely get the item
+        item = ((PyListObject *)p)->ob_item[i];
         printf("Element %zd: ", i);
         if (PyBytes_Check(item)) {
             print_python_bytes(item);
-        } else {
-            printf("(%s)\n", Py_TYPE(item)->tp_name);  // Print the type of the item
         }
     }
 }
@@ -54,11 +50,10 @@ void print_python_bytes(PyObject *p) {
     printf("  size: %zd\n", size);
     printf("  trying string: %s\n", str ? str : "(null)");
 
-    size = (size < 10) ? size : 10;
-    printf("  first %zd bytes: ", size);
-    for (Py_ssize_t i = 0; i < size; i++) {
+    printf("  first %zd bytes: ", size < 10 ? size : 10);
+    for (Py_ssize_t i = 0; i < (size < 10 ? size : 10); i++) {
         printf("%02hhx", (unsigned char)str[i]);
-        if (i < size - 1) {
+        if (i < (size < 10 ? size - 1 : 9)) {
             printf(" ");
         }
     }
