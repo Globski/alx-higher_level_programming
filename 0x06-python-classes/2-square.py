2898f57142a5a55628b7fa4e039e9b411b938ef3
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-12 18:57:23 -0700

    Implement size validation in Square class constructor.

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,12 +4,12 @@
 
 class Square:
     """Class that defines a square by its size."""
-    
+
     def __init__(self, size=0):
         """Initialize a new Square with a given size."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+    
         self.__size = size
