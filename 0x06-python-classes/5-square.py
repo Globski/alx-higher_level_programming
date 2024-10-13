ef4210eca518fa91a864f7dc005c97d52281ae31
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-12 19:13:03 -0700

    Add my_print method to Square class for visual representation.

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Define a class Square with a private attribute size, area calculation, and printing."""
+"""Define a class Square with a private attribute size, area, and printing."""
 
 
 class Square:
