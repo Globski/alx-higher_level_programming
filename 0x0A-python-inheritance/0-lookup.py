4e7b105aad1c070e62d98b1354d7b98219b07794
globski <gloriaogunsemore.com>
2024-10-15 11:33:35 +0000

    Add 0-lookup.py: Implement function to retrieve a list of attributes and methods of an object, adhering to project specifications

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Define a function to lookup attributes and methods of an object.
+"""
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+    
+    Parameters:
+    obj (object): The object to inspect.
+
+    Returns:
+    list: A list of attributes and methods of the object.
+    """
+    return dir(obj)
