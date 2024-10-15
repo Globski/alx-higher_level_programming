2c721a89c685d790a5512a457706c94db2bec9e5
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-15 05:21:25 -0700

    Add Rectangle class that inherits from BaseGeometry with validation.

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -26,3 +26,15 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """
+        Returns the area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
