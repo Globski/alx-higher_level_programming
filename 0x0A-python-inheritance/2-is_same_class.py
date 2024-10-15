9656886e990daa55715fe753bee2a35657aaa596
globski <gloriaogunsemore.com>
2024-10-15 11:48:15 +0000

    Add is_same_class function to check exact instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Define a function to check if an object is exactly an instance of a specified class.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if the object is exactly an instance of the specified class.
+
+    Parameters:
+    obj (object): The object to check.
+    a_class (type): The class to compare with.
+
+    Returns:
+    bool: True if obj is exactly an instance of a_class, otherwise False.
+    """
+    return type(obj) is a_class
