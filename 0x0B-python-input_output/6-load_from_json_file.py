ef7ab763ad13b5e7fe8bd1607def626db028b9ff
globski <gloriaogunsemore.com>
2024-10-16 21:07:20 +0000

    Add 6-load_from_json_file.py: Function to create object from JSON file with documentation

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a function to create an object from a JSON file."""
+
+import json
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file.
+
+    Args:
+        filename (str): The name of the file containing the JSON string.
+
+    Returns:
+        object: The Python object represented by the JSON string.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
