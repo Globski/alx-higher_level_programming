5c9af8c8c7923fb30cab760ad8524662290d3310
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-16 14:24:35 -0700

    Add 7-add_item.py: Add arguments to a list and save to JSON file.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Adds all arguments to a Python list and saves them to a file."""
 
-
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 FILENAME = "add_item.json"
+
 
 def main():
     """Main function to load, add, and save items to a JSON file."""
@@ -17,6 +17,7 @@ def main():
 
     items.extend(sys.argv[1:])
     save_to_json_file(items, FILENAME)
+
 
 if __name__ == "__main__":
     main()
