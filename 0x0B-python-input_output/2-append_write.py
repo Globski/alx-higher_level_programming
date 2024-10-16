d207ff152de1ee5acf2b5170ef78a6fdad1fc169
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-16 13:54:41 -0700

    Add 2-append_write.py: Function to append a string to a text file.

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Defines a function to append a string to a text file."""
 
+
 def append_write(filename="", text=""):
-    """Appends a string at the end of a text file (UTF-8) and returns the number of characters added.
+    """Appends a string at the end of a text file (UTF-8) and
+    returns the number of characters added.
 
     Args:
-        filename (str): The name of the file to append to. Defaults to an empty string.
-        text (str): The string to append to the file. Defaults to an empty string.
+        filename (str): The name of the file to append to.
+        Defaults to an empty string.
+        text (str): The string to append to the file.
+        Defaults to an empty string.
 
     Returns:
         int: The number of characters added to the file.
