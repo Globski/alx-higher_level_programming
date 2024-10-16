775ccddbceae000c6e12a4fe8023ad10a3209079
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-16 13:45:39 -0700

    Add 0-read_file.py: Function to read and print a text file.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,8 @@ def read_file(filename=""):
     """Reads a text file (UTF-8) and prints its contents to stdout.
 
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
+        filename (str): The name of the file to read.
+        Defaults to an empty string.
     """
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end="")
