46e616db8f70e72cd7d823dc4b895c1118c75e3a
globski <gloriaogunsemore.com>
2024-10-16 21:27:28 +0000

    Add 8-class_to_json.py: Function to serialize class attributes to JSON with documentation

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a function that returns the dictionary description for JSON serialization of an object."""
+
+
+def class_to_json(obj):
+    """Return the dictionary description for JSON serialization of an object.
+
+    Args:
+        obj (object): An instance of a class.
+
+    Returns:
+        dict: A dictionary representation of the object's serializable attributes.
+    """
+    return {key: value for key, value in obj.__dict__.items() if 
+            isinstance(value, (list, dict, str, int, bool))}
