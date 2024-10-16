9648129bbcefcd81c8e85c373396d7fbdbcbff9a
globski <gloriaogunsemore.com>
2024-10-16 20:53:22 +0000

    Add 2-append_write.py: Function to append a string to a text file with documentation

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a function to append a string to a text file."""
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file (UTF-8) and returns the number of characters added.
+
+    Args:
+        filename (str): The name of the file to append to. Defaults to an empty string.
+        text (str): The string to append to the file. Defaults to an empty string.
+
+    Returns:
+        int: The number of characters added to the file.
+    """
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
