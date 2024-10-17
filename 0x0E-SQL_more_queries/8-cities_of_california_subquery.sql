a0cd702365568bc3f5f571e1a4a07f6f629961d6
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-17 07:28:09 -0700

    Add 8-cities_of_california_subquery.sql: List cities in California.

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,4 @@
--- Lists all cities in California from the database hbtn_0d_usa
--- The states table is queried to find the state_id for California,
--- then used to select cities with that state_id, sorted by city id.
+-- List all cities in California from the database hbtn_0d_usa
 SELECT * FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
