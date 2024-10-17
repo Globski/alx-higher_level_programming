2185c5f600c0a89428f713bae0fbb7cb215760a2
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-17 07:19:53 -0700

    Add 101-not_a_comedy.sql: List all shows without the genre Comedy.

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,12 @@
+-- Lists all shows without the genre Comedy
+-- Results are sorted in ascending order by the show title
+
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.id NOT IN (
+    SELECT tv_show_genres.show_id
+    FROM tv_show_genres
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY tv_shows.title ASC;
