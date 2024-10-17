f797ec418d26039f8166a61f6b039bf023e6bc30
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-17 06:53:12 -0700

    Add 2-create_read_user.sql: Create database and user with SELECT privilege.

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,14 @@
+-- Creates the database hbtn_0d_2 and user user_0d_2
+-- Grants user_0d_2 SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
+
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT
+    ON `hbtn_0d_2`.*
+    TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
