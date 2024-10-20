689c35e47616af2eb6a4bcd19215f74c35171d43
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-19 23:42:19 -0700

    Add LockedClass to restrict dynamic attribute creation: Implemented a class that only allows the creation of the attribute first_name.

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,23 +2,24 @@
 """
 LockedClass Module
 
-This module defines the LockedClass, which restricts the creation of 
+This module defines the LockedClass, which restricts the creation of
 instance attributes to only one specific attribute named 'first_name'.
-This is achieved by using the __slots__ mechanism, which prevents 
-dynamic addition of attributes to instances of the class, thus 
+This is achieved by using the __slots__ mechanism, which prevents
+dynamic addition of attributes to instances of the class, thus
 conserving memory and improving performance.
 """
+
 
 class LockedClass:
     """
     LockedClass
 
-    A class that prevents the dynamic creation of instance attributes 
-    other than 'first_name'. This is useful for enforcing a strict 
+    A class that prevents the dynamic creation of instance attributes
+    other than 'first_name'. This is useful for enforcing a strict
     attribute structure within instances.
 
     Attributes:
-        first_name (str): The only allowed attribute that can be set 
+        first_name (str): The only allowed attribute that can be set
                           dynamically.
     """
 
