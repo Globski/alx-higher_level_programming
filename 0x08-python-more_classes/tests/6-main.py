d88984245bdc82b40d60ee78393c3a60e901e78a
globski <gloriaogunsemore.com>
2024-10-21 18:08:27 +0000

    Add 6-main.py: Test script for rectangle instance counting

---
+++ 0x08-python-more_classes/tests/6-main.py
@@ -0,0 +1,11 @@
+guillaume@ubuntu:~/0x08$ cat 6-main.py
+#!/usr/bin/python3
+Rectangle = __import__('6-rectangle').Rectangle
+
+my_rectangle_1 = Rectangle(2, 4)
+my_rectangle_2 = Rectangle(2, 4)
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_1
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_2
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
