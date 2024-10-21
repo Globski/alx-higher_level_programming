0ad6fc36c37ecba5c83298b8c2117927f75fbeb8
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-21 10:57:23 -0700

    Add 7-rectangle.py: Implement customizable print symbol for Rectangle class

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -77,8 +77,8 @@ class Rectangle:
         """Return the string representation of the rectangle."""
         if self.__width == 0 or self.__height == 0:
     return "\n".join(
-        str(self.print_symbol) * self.__width
-        for _ in range(self.__height))
+            str(self.print_symbol) * self.__width for _ in range(self.__height)
+        )
 
 
     def __repr__(self):
