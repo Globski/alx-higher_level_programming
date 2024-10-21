7527335779e68fb87a8d63442523529ec721dba9
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-21 11:26:37 -0700

    Add 3-rectangle.py: Implement string representation for Rectangle class

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -74,7 +74,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join("#" * self.__width for _ in range(self.__height))
-
-    def __repr__(self):
-        """Return the official string representation of the rectangle."""
-        return f"Rectangle({self.width}, {self.height})"
